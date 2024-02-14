d4a53965de449591bdc2fe404d3d74e14a1177ac
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-02-14 22:16:19 +0000

    Finally fixed all of the linter problems

--- package-info.java
+++ package-info.java
@@ -1,8 +1,5 @@
 /**
-* This is the package-info.java file.
-*
-* @author  Ms Raffin
-* @version 1.0
-* @since   2024-02-12
-*/
+ * @version 1.0
+ * @since 2024-02-12
+ */
 package ca.immaculata.ics4u;
