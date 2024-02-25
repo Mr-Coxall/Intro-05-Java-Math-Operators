0f95483abb36efc7276f1ec78fa2013b3f7ca257
Patrick Coxall <mr.coxall@mths.ca>
2024-02-25 12:10:05 -0500

    Use constants and "magical" numbers

--- MathOperators.java
+++ MathOperators.java
@@ -6,6 +6,12 @@
  * @since 2024-02-12
  */
 public final class MathOperators {
+  /**
+   * Number constants.
+  */
+  public static final int NUMBER_TWO = 2;
+  public static final int NUMBER_FORTY = 40;
+  
   /** Private constructor to prevent instantiation of this utility class. */
   private MathOperators() {
     // Prevents instantiation.
@@ -18,30 +24,6 @@ public final class MathOperators {
    */
   public static void main(final String[] args) {
     // Does addition and outputs the result to the console.
-    System.out.println("2 + 40 = " + (2 + 40));
-
-    // Does subtraction and outputs the result to the console.
-    System.out.println("6 - 9 = " + (6 - 9));
-
-    // Does multiplication and outputs the result to the console.
-    System.out.println("45 * 6 = " + (45 * 6));
-
-    // Does division without decimal and outputs the result to the console.
-    System.out.println("81 / 9 = " + (81 / 9));
-
-    // Does division (float) and outputs the result to the console.
-    System.out.println("Float\n10 / 6 = " + (10f / 6f));
-
-    // Does division (double) and outputs the result to the console.
-    System.out.println("Double\n100 / 3 = " + (100d / 3d));
-
-    // Does exponents and outputs the result to the console.
-    System.out.println("4^7 = " + Math.pow(4, 7));
-
-    // Does square root and outputs the result to the console.
-    System.out.println("√64 = " + Math.sqrt(64));
-
-    // Does cube root and outputs the result to the console.
-    System.out.println("∛125 = " + Math.cbrt(125));
+    System.out.println("2 + 40 = " + (NUMBER_TWO + NUMBER_FORTY));
   }
 }
