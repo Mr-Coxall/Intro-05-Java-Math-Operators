78c1a8831af1c093954c56a7e03d650c04116a6a
Patrick Coxall <mr.coxall@mths.ca>
2024-02-25 18:17:45 +0000

    linting redo, with 4 space indenting

--- MathOperators.java
+++ MathOperators.java
@@ -1,33 +1,46 @@
 /**
- * Various ways to use math operators in Java.
- *
- * @author Kevin Csiffary
- * @version 1.0
- * @since 2024-02-12
- */
+* Various ways to use math operators in Java.
+*
+* @author Kevin Csiffary
+* @version 1.0
+* @since 2024-02-12
+*/
+
+/**
+* This is the microwave program.
+*/
 final class MathOperators {
-  /**
-   * Number constants, 2.
-   */
-  static final int NUMBER_TWO = 2;
+    /**
+    * Number 2.
+    */
+    static final int NUMBER_TWO = 2;
 
-  /**
-   * Number constants, 40.
-   */
-  static final int NUMBER_FORTY = 40;
+    /**
+    * Number 40.
+    */
+    static final int NUMBER_FORTY = 40;
 
-  /** Private constructor to prevent instantiation of this utility class. */
-  private MathOperators() {
-    // Prevents instantiation.
-  }
+    /**
+    * Prevent instantiation
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private MathOperators() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
 
-  /**
-   * Create main function.
-   *
-   * @param args Command line arguments.
-   */
-  public static void main(final String[] args) {
-    // Does addition and outputs the result to the console.
-    System.out.println("2 + 40 = " + (NUMBER_TWO + NUMBER_FORTY));
-  }
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+        // Does addition and outputs the result to the console.
+        System.out.println("2 + 40 = " + (NUMBER_TWO + NUMBER_FORTY));
+
+        System.out.println("\nDone.");
+    }
 }
